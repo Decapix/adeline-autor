cf05adb149846730ef36fdca76243af9ee576bf6
decapix <decapixd@gmail.com>
2024-11-19 11:00:14 +0100

    storage media

--- src/adeline/settings.py
+++ src/adeline/settings.py
@@ -192,7 +192,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
 
-django_heroku.settings(locals())
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # AWS_S3_ENDPOINT_URL = "https://adeline-site-media.s3.fr-par.scw.cloud" # os.environ.get('CLOUDCUBE_URL')
@@ -229,3 +228,5 @@ AWS_STORAGE_BUCKET_NAME = "adeline-site-media"
 AWS_S3_REGION_NAME = 'fr-par'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_S3_FILE_OVERWRITE = False
+
+django_heroku.settings(locals())
